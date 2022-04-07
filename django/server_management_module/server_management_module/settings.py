15745196862230a850c7a2aadbfb49f82663d993
besonia.ma13 <besonia.ma13@s.msumain.edu.ph>
2022-04-07 09:11:30 +0800

    change database location

--- django/server_management_module/server_management_module/settings.py
+++ django/server_management_module/server_management_module/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
 #BASE_DIR = '/home/malware/docs/python/django/'
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
