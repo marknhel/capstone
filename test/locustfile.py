8db4e77a380236771352ddfe9904634b9b524fcb
besoniama13 <besonia.ma13@s.msumain.edu.ph>
2023-01-15 10:45:44 +0800

    Backup

--- test/locustfile.py
+++ test/locustfile.py
@@ -3,5 +3,5 @@ from locust import HttpUser, task
 class HelloWorldUser(HttpUser):
     @task
     def hello_world(self):
-        self.client.get("/register")
+#        self.client.get("/register")
         self.client.get("/")
